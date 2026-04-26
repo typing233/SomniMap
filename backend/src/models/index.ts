8041275781a46bea0e358cfd93c084666636e8d5
typing233 <helloe341@gmail>
2026-04-26 21:30:10 +0800

    1

--- backend/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { User } from './User';
-export { Dream } from './Dream';
-export { DreamAnalysis } from './DreamAnalysis';
-export { Tag } from './Tag';
-export { DreamPattern, DreamMotif, EmotionSnapshot } from './DreamPattern';
--- backend/src/routes/index.ts
+++
@@ -1,4 +0,0 @@
-export { authRoutes } from './authRoutes';
-export { dreamRoutes } from './dreamRoutes';
-export { tagRoutes } from './tagRoutes';
-export { statisticsRoutes } from './statisticsRoutes';
